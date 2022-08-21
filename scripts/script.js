a056d0180a220e5c9c1ecb7faad8d11f7bee8804
aliaa gheis <aliaagheis@gmail.com>
2022-08-21 11:02:40 +0200

    style: apply pretterier, divide code to classes

--- scripts/script.js
+++ scripts/script.js
@@ -25,7 +25,7 @@ class Course {
         this.price = courseData.price;
     }
 
-    getCourseNode () {
+    getCourseNode() {
         const courseNode = createElementE("div", { "class": "carousel-item  course search-courses" });
         courseNode.appendChild(this.#getImageNode());
         courseNode.appendChild(this.#getTitleNode());
@@ -46,8 +46,8 @@ class Course {
 
     #getTitleNode() {
         const courseLinkNode = createElementE(
-            'a', 
-            {"href": `https://www.udemy.com/course/${this.titleID}` }, 
+            'a',
+            { "href": `https://www.udemy.com/course/${this.titleID}` },
             this.title
         );
         const courseNameNode = createElementE('h4', { 'class': 'course-name' });
@@ -57,36 +57,36 @@ class Course {
         return courseNameNode;
     }
 
-    #getInstructorsNode () {
+    #getInstructorsNode() {
         return createElementE(
             'span',
-            {'class': 'utiltiy-comment instructor'},
+            { 'class': 'utiltiy-comment instructor' },
             this.instructors[0].name
         );
     }
 
-    #getStarsNode () {
+    #getStarsNode() {
 
         const starsContainer = createElementE('div', { 'class': 'stars' });
-    
+
         for (let i = 1; i <= 5; i++) {
             //make star element as full star is the default
             let star = createElementE('span', { 'class': 'material-symbols-outlined' }, 'star');
-    
+
             if (Math.ceil(this.rating) < i)
                 star.classList.add('empty_star');
             else if (this.rating < i)
                 star.innerText = 'star_half';
-    
+
             //add star to star container
             starsContainer.appendChild(star);
         }
-    
+
         return starsContainer;
-        
+
     }
 
-    #getRatingNode () {
+    #getRatingNode() {
         const courseRatingNode = createElementE('div', { 'class': 'rating-description' });
         const courseRatingInfoNode = createElementE('span', { 'class': 'rating' }, this.rating);
         const starsContainerNode = this.#getStarsNode();
@@ -99,137 +99,128 @@ class Course {
         return courseRatingNode;
     }
 
-    #getPriceNode () {
+    #getPriceNode() {
         return createElementE('span', { 'class': 'price' }, `E£${this.price}`);
     }
 
 }
 
+class Category {
+    constructor(categoryData) {
+        this.title = categoryData.title.slice(15);
+        this.titleID = this.title.replace(/ /g, '-');
+        this.header = categoryData.header;
+        this.description = categoryData.description;
+        this.courses = categoryData.courses;
+    }
 
-/* category to Dom functions*/
-const addCategoryTab = (title, titleID) => {
-    const categoryLinkNode = createElementE('button', {
-        'class': 'nav-link',
-        'id' : `nav-${titleID}-tab`,
-        'data-bs-toggle': "tab",
-        'data-bs-target': `#nav-${titleID}`,
-        'aria-controls':`nav-${titleID}`,
-        'type':"button",
-        'role':"tab",
-        'aria-selected':'false'
-    }, title);
-    
-    
-    document.querySelector('#category-nav .nav').appendChild(categoryLinkNode);
+    makeCategoryNode() {
+        this.#addCategoryTabLink();
+
+        const container = this.#getTabContainer();
+
+        this.#addCategoryDetails(container);
+
+        const coursesContainer = this.#makeCarsoul(container);
+
+        this.#addCourses(coursesContainer);
+    }
+
+    #addCategoryTabLink() {
+        const categoryLinkNode = createElementE('button', {
+            'class': 'nav-link',
+            'id': `nav-${this.titleID}-tab`,
+            'data-bs-toggle': "tab",
+            'data-bs-target': `#nav-${this.titleID}`,
+            'aria-controls': `nav-${this.titleID}`,
+            'type': "button",
+            'role': "tab",
+            'aria-selected': 'false'
+        }, this.title);
+
+        document.querySelector('#category-nav .nav').appendChild(categoryLinkNode);
+    }
+
+    #getTabContainer() {
+        const tab = createElementE('div', {
+            'class': "tab-pane show",
+            'id': `nav-${this.titleID}`,
+            'role': "tabpanel",
+            'aria-labelledby': `nav-${this.titleID}-tab`,
+            'tabindex': 0
+        });
+
+        const container = createElementE('div', {
+            'class': "myContainer course-content",
+            'id': `courses-${this.titleID}`
+        });
+
+        tab.appendChild(container);
+        document.querySelector('.category-tab').appendChild(tab);
+
+        return container;
+    }
+
+    #addCategoryDetails(container) {
+        container.appendChild(createElementE('h3', {}, this.header));
+        container.appendChild(createElementE('p', {}, this.description));
+        container.appendChild(createElementE('a', {
+            'class': 'btn',
+            'href': '/'
+        }, `Explore ${this.title}`));
+    }
+
+    #makeCarsoulBtn(functionality) {
+        const btn = createElementE('button', {
+            "class": `carousel-control-${functionality}`,
+            "type": "button",
+            "data-bs-target": `#courses-${this.titleID}`,
+            'data-bs-slide': functionality
+        });
+
+        const circle = createElementE('span', { 'class': 'circle' });
+        const icn = createElementE(
+            'span',
+            { 'class': 'material-symbols-outlined' },
+            functionality == 'prev' ? 'navigate_before' : 'navigate_next'
+        );
+
+        const accessibility = createElementE('span', { 'class': 'visually-hidden' }, functionality);
+
+        circle.appendChild(icn);
+        btn.appendChild(circle);
+        btn.appendChild(accessibility);
+        return btn;
+    }
+
+    #makeCarsoul(container) {
+        const carousel = createElementE('div', {
+            'class': 'carousel',
+            'data-bs-ride': "carousel"
+        });
+
+        const coursesContainer = createElementE('div', {
+            'class': "carousel-inner row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5"
+        });
+
+        carousel.appendChild(coursesContainer);
+        carousel.appendChild(this.#makeCarsoulBtn('prev'));
+        carousel.appendChild(this.#makeCarsoulBtn('next'));
+
+        /* add it in container */
+        container.appendChild(carousel);
+
+        /* return where the courses should append */
+        return coursesContainer;
+    }
+
+    #addCourses(container) {
+        this.courses.forEach((courseData) => {
+            let newCourse = new Course(courseData);
+            container.appendChild(newCourse.getCourseNode());
+        });
+    }
 }
-
-const btnIcon = (functionality, titleID) => {
-    const btn = createElementE('button', {
-        "class":`carousel-control-${functionality}`, 
-        "type":"button", 
-        "data-bs-target":`#courses-${titleID}`,
-        'data-bs-slide': functionality
-    });
-
-    const circle = createElementE('span', {'class':'circle'});
-    const icn = createElementE(
-        'span', 
-        {'class':'material-symbols-outlined'},
-        functionality == 'prev' ? 'navigate_before' : 'navigate_next'
-    );
-
-    const accessibility = createElementE('span', {'class':'visually-hidden'}, functionality);
-
-    circle.appendChild(icn);
-    btn.appendChild(circle);
-    btn.appendChild(accessibility);
-    return btn;
-}
-
-const inializeCarsoul = (container, titleID) => {
-    const carousel = createElementE('div', {
-        'class': 'carousel',
-        'data-bs-ride': "carousel"
-    });
-    
-    const row = createElementE('div', {
-        'class': "carousel-inner row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-5"
-    });
-
-    carousel.appendChild(row);
-    carousel.appendChild(btnIcon('prev', titleID));
-    carousel.appendChild(btnIcon('next', titleID));
-
-    /* add it in container */
-    container.appendChild(carousel);
-
-    /* return where the courses should append */
-    return row;
-}
-
-const addCategoryDetails = (container, title, category) => {
-    container.appendChild(createElementE('h3', {}, category.header));
-    container.appendChild(createElementE('p', {}, category.description));
-    container.appendChild(createElementE('a', {
-        'class': 'btn',
-        'href': '/'
-    }, `Explore ${title}`));
-}
-
-const inializeCategory = (title, titleID, category) => {
-
-    const tab = createElementE('div', {
-        'class': "tab-pane show",
-        'id': `nav-${titleID}`,
-        'role': "tabpanel",
-        'aria-labelledby': `nav-${titleID}-tab`,
-        'tabindex': 0
-    });
-    
-    const container = createElementE('div', {
-        'class': "myContainer course-content",
-        'id':  `courses-${titleID}`
-    });
-    
-    /* 1- add details of category to container */
-    addCategoryDetails(container, title, category);
-    
-    /* 2- inialize the carsoul and get the element */
-    const coursesContainer = inializeCarsoul(container, titleID);
-    
-    /* append all this to page */
-    tab.appendChild(container);
-    document.querySelector('.category-tab').appendChild(tab);
-
-    return coursesContainer;
-}
-
-const addCategory = (category) => {
-
-    /* get title of category */
-    const title = category.title.slice(15);
-    const titleID = title.replace(' ', '-');
-
-    //1- and category link to tab
-    addCategoryTab(title, titleID);
-
-    /* 
-        2- inialize courses container
-            2.1- inialize all containers, tabs, carsouls,
-            2.2- add category details to container
-            2.3- add to html Dom
-    */
-    const coursesContainer = inializeCategory(title,titleID,category);
-    
-    //3- add all course to container
-    category.courses.forEach((courseData) => {
-        let newCourse = new Course(courseData);
-        coursesContainer.appendChild(newCourse.getCourseNode());
-        //coursesContainer.appendChild(addCourse(course)); 
-    });
-
-}
-
 
 
 /* for searching */
@@ -265,25 +256,25 @@ const search = (e) => {
     carousel.children[2].style.display = displayNext;
 } */
 
-async function scrollAnimation (element) {
+async function scrollAnimation(element) {
     /* data attributes */
     const target = element.getAttribute('data-bs-target');
     const behavior = element.getAttribute('data-bs-slide');
-    
+
     /* element getters */
     const carousel = document.querySelector(`${target} .carousel`);
     const row = carousel.children[0];
-    
+
     /* scroll calculation */
-    const step = Math.max(row.offsetWidth - row.children[0].offsetWidth,row.children[0].offsetWidth);
+    const step = Math.max(row.offsetWidth - row.children[0].offsetWidth, row.children[0].offsetWidth);
     let left = row.scrollLeft;
     left += behavior == 'next' ? +step : -step;
-    
+
     row.scroll({
         left: (left),
         behavior: 'smooth'
-    }) 
-    
+    })
+
 }
 
 
@@ -291,26 +282,30 @@ async function scrollAnimation (element) {
 /* call first after fetch data from api */
 const inializer = (data) => {
 
-    /*add all courses in  */
-    for (category in data) {
-        addCategory(data[category]);
+    /*add all categroies to html dom  */
+    for (categoryData in data) {
+        const cat = new Category(data[categoryData]);
+        cat.makeCategoryNode();
     }
- 
-    //inial all cousres display none
-    document.querySelector('#category-nav .nav-link').classList.add('active');
-    document.querySelector('#category-nav .nav-link').setAttribute('aria-selected', 'true');
+
+    //activate first tab link
+    const firstTabLink = document.querySelector('#category-nav .nav-link');
+    firstTabLink.classList.add('active');
+    firstTabLink.setAttribute('aria-selected', 'true');
+
+    //activate first tab
     document.querySelector('.tab-pane').classList.add('active');
-    
-    
+
+
     //add events for searching
     document.getElementById("search").addEventListener("submit", search);
     document.getElementById("search-bar").addEventListener("keyup", search);
 
 
     //add event for scroll
-   document.querySelectorAll('.carousel button').forEach((element)=> {
+    document.querySelectorAll('.carousel button').forEach((element) => {
         element.addEventListener('click', () => scrollAnimation(element));
-   });
+    });
 }
 
 /* api to get all course */
